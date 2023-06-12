f1bb36920722c1ce762a03010431139f272f2b5e
caiyinhua <caiyinhua@cloudstream-tech.com>
2023-06-12 21:33:55 +0800

    add check novels update api

--- server/v1/novel.go
+++ server/v1/novel.go
@@ -2,6 +2,9 @@ package v1
 
 import (
 	"VpnScrapy/crawl/novel"
+	"VpnScrapy/crawl/novel/hulk"
+	"fmt"
+	"net/http"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
@@ -10,7 +13,42 @@ import (
 func LaunchNovel(group *gin.RouterGroup) {
 	novelGroup := group.Group("novel")
 	{
-		novelGroup.GET("/crawlNovel", crawlNovel)
+		novelGroup.GET("/crawl_novel", crawlNovel)
+		novelGroup.GET("/check_update", checkNovelsUpdate)
+	}
+}
+
+// 检查
+func checkNovelsUpdate(c *gin.Context) {
+	novelScrapy := new(hulk.HulkScrapy)
+	novelUpdateResult := "["
+	checkNovelUpdate(novelScrapy, "spy-mage-system", &novelUpdateResult, true)
+	checkNovelUpdate(novelScrapy, "necropolis-immortal", &novelUpdateResult, true)
+	checkNovelUpdate(novelScrapy, "cultivation-online-novel", &novelUpdateResult, true)
+	checkNovelUpdate(novelScrapy, "the-experimental-log-of-the-crazy-lich", &novelUpdateResult, true)
+	checkNovelUpdate(novelScrapy, "invincible-divine-dragons-cultivation-system", &novelUpdateResult, true)
+	checkNovelUpdate(novelScrapy, "trial-marriage-husband-need-to-work-hard", &novelUpdateResult, false)
+	novelUpdateResult += "]"
+	c.String(http.StatusOK, novelUpdateResult)
+}
+
+func checkNovelUpdate(hulkScrapy *hulk.HulkScrapy, novelId string, result *string, appendComma bool) {
+	novelChapterUrl := fmt.Sprintf(novel.UrlHulkNovelDomain+novel.PathChapterList, novelId)
+	content, err := novel.Request(novelChapterUrl, "GET")
+	if err != nil {
+		panic(err)
+	}
+	doc := hulkScrapy.CreateParseDoc(string(content))
+	chapterList := hulkScrapy.ParseNovelChapterList(doc)
+
+	if len(chapterList) != 0 {
+		chapterSize := len(chapterList)
+		*result += fmt.Sprintf(`{"novelId": "%s", "index": %d, "lastest": "%s"}`, novelId, chapterSize, chapterList[chapterSize-1].Name)
+	} else {
+		*result += fmt.Sprintf(`{"novelId": "%s", "lastest": "none"}`, novelId)
+	}
+	if appendComma {
+		*result += ","
 	}
 }
 
